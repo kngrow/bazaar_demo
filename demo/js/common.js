66c619ddf5fa5b64ef97b6b1be331c54d75606b8
YusukeYamasaki <ymc9.knit.p23@gmail.com>
2014-10-13 02:11:07 +0900

    no

--- demo/js/common.js
+++ demo/js/common.js
@@ -1,19 +1,88 @@
 /*▼light boxで商品説明を出す*/
-$(function(){
-	$(".item li a").each(function() {
-		//aタグ内にimgタグがあるか？
-		if( $(this).find('img')) {
-			$(this).attr( "data-lightbox", "image-1" ); // 画像リンクの場合だけ属性を追加する
-		}
 
-		$(this).on("click",function(){
-			var items = $(this).find(".items").html();
-			$(".detail_text").html(items);
-		});
+
+$(function(){
+			//model
+
+			$.get('/reji/items/metaread')
+						.done(function(data){
+							debugger;
+
+						 	var all = Backbone.Model.extend({
+
+							});
+
+
+							//全部の商品のデータ
+							for(var i = 0 ; i < data.length ; i++){
+							var allitem = new all({
+								id : data[i].Item.id, //商品id
+								name : data[i].Item.item_name, //商品の名前
+								price : data[i].Item.item_price, //値段
+								detail : data[i].Item.item_detail , //説明
+								photo : data[i].Item.item_photo , //写真の名前
+								photodir : data[i].Item.item_photo_dir ,		//写真の場所
+								// /regi/img/item/item_photo/'item_photo_dir'/'item_photo でとれるはず
+								 stock :data[i].Item.item_stock , //個数
+								category : data[i].Category.category_name //カテゴリの名前
+							});
+							// console.log( allitem.toJSON() );
+
+
+							//view
+							var topitemsView = Backbone.View.extend({
+								template : _.template ($("#item_temp").html() ),
+								// events : {
+								// 	'click .items' : ''
+								// },
+								render : function(){
+									var top_temp = this.template( this.model.toJSON() );
+									this.$el.html (top_temp);
+									return this ;
+								} ,
+								lightbox : function(){
+									var items = $(this).find(".items").html();
+									$(".detail_text").html(items);
+								}
+							});
+
+
+								var topvw  = new  topitemsView({ model : allitem});
+
+								console.log(topvw.render().el );
+								$("#accessories").append(topvw.render().el);
+							}
+						});
+});
+
+
+$(function(){
+
+
+
+
+	$(document).on('click touchend','' ,function(){
+		console.log("aaa");
+
 	});
 
-	$(".lb-data").css({"display":"none !important"});
-	$(".lb-number").css({"display":"none !important"});
+	// $(".item li a").each(function() {
+		//aタグ内にimgタグがあるか？
+	// 	if( $(this).find('img')) {
+	// 		$(this).attr( "data-lightbox", "image-1" ); // 画像リンクの場合だけ属性を追加する
+	// 	}
+	//
+	// 	$(this).on("touchend click",function(){
+	// 		var items = $(this).find(".items").html();
+	// 		$(".detail_text").html(items);
+	// 	});
+	// });
+	$(document).on("click touhend" , ".items" , function(){
+		// var items =
+	});
+
+	// $(".lb-data").css({"display":"none !important"});
+	// $(".lb-number").css({"display":"none !important"});
 });
 
 /*▼画面上部に戻るボタンのアニメーション*/
@@ -73,49 +142,7 @@ function fadeout(){
 	)
 }
 
-$(function(){
-			//model
 
-			$.get('/reji/items/metaread')
-						.done(function(data){
-							debugger;
-
-						 	var all = Backbone.Model.extend();
-
-
-							//全部の商品のデータ
-							for(var i = 0 ; i < data.length ; i++){
-							var allitem = new all({
-								id : data[i].Item.id, //商品id
-								name : data[i].Item.item_name, //商品の名前
-								price : data[i].Item.item_price, //値段
-								detail : data[i].Item.item_detail , //説明
-								photo : data[i].Item.item_photo , //写真の名前
-								photodir : data[i].Item.item_photo_dir ,		//写真の場所
-								// /regi/img/item/item_photo/'item_photo_dir'/'item_photo でとれるはず
-								 stock :data[i].Item.item_stock //個数
-							});
-							// console.log( allitem.toJSON() );
-
-
-							//view
-							var topitemsView = Backbone.View.extend({
-								template : _.template ($("#item_temp").html() ),
-								render : function(){
-									var top_temp = this.template( this.model.toJSON() );
-									this.$el.html (top_temp);
-									return this ;
-								}
-							});
-
-
-								var topvw  = new  topitemsView({ model : allitem});
-
-								console.log(topvw.render().el );
-								$("#item").append(topvw.render().el);
-							}
-						});
-});
 
 
 
