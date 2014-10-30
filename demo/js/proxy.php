c27691c2ad8440856f35efdb7ef9d206422a534c
Kentaro Hagiwara <rastyrein.ken.091012@gmail.com>
2014-10-30 14:54:57 +0900

    url変更しました

--- demo/js/proxy.php
+++ demo/js/proxy.php
@@ -2,8 +2,10 @@
 header("Content-Type:application/json");
 /* URLが指すXMLコンテンツを取得して返す（Ajaxの中継用）*/
 if(isset($_POST['URL'])){
-$url=$_POST['URL'];
-echo file_get_contents($url);
+    if("hogehoge"==$_POST['URL']){
+        $url="http://metaregi.me-ta-tag.com/items/metaread";
+        echo file_get_contents($url);
+    }
 }
 /* curlが使えるならこうする（xampp liteには無い）
 $ch = curl_init($_POST['URL']);
